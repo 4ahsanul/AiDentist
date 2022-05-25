a1c816cb2eab5b30fb489e363093145edf98ee46
Filfimo Yulfiz Ahsanul Hulqi <58451478+4ahsanul@users.noreply.github.com>
2022-05-25 19:21:23 +0700

    Coba SharedPreference antar Activity dan Fragment

--- MD/app/src/main/java/com/fimo/aidentist/ui/profile/ProfileFragment.kt
+++ MD/app/src/main/java/com/fimo/aidentist/ui/profile/ProfileFragment.kt
@@ -1,22 +1,44 @@
 package com.fimo.aidentist.ui.profile
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.fimo.aidentist.R
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import com.fimo.aidentist.MainActivity
+import com.fimo.aidentist.databinding.FragmentProfileBinding
+import com.fimo.aidentist.helper.Constant
 import com.fimo.aidentist.helper.PreferenceHelper
+import com.fimo.aidentist.ui.auth.LoginActivity
 
 class ProfileFragment : Fragment() {
-    lateinit var sharedPref: PreferenceHelper
+    private lateinit var sharedPref: PreferenceHelper
+    private var _binding: FragmentProfileBinding? = null
+    private val binding get() = _binding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+        _binding = FragmentProfileBinding.inflate(layoutInflater, container, false)
+        return binding?.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding?.buttonLogout?.setOnClickListener {
+            activity?.let {
+                sharedPref.put(Constant.PREF_IS_LOGIN, false)
+                val intent = Intent(it, LoginActivity::class.java)
+                it.startActivity(intent)
+                Toast.makeText(activity, "LOGOUT SUCCESS", Toast.LENGTH_SHORT).show()
+//                sharedPref.clear()
+//                sharedPref.clear()
+//                activity?.finish()
+            }
+        }
+    }
 }
