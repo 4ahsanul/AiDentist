0f72b34e01b2cecb2e95fbbc53804483f6b06c33
Filfimo Yulfiz Ahsanul Hulqi <58451478+4ahsanul@users.noreply.github.com>
2022-05-31 20:22:10 +0700

    Persiapan mengganti Doctor Fragment menjadi Activity

---
+++ MD/app/src/main/java/com/fimo/aidentist/ui/doctor/DoctorActivity.kt
@@ -0,0 +1,12 @@
+package com.fimo.aidentist.ui.doctor
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.fimo.aidentist.R
+
+class DoctorActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_doctor)
+    }
+}
