5a681f3e734cf73a1d2039f05295f0d7d7bc9bef
Filfimo Yulfiz Ahsanul Hulqi <58451478+4ahsanul@users.noreply.github.com>
2022-06-03 11:01:42 +0700

    Membuat tampilan message

--- MD/app/src/main/java/com/fimo/aidentist/ui/home/HomeFragment.kt
+++ MD/app/src/main/java/com/fimo/aidentist/ui/home/HomeFragment.kt
@@ -15,33 +15,13 @@ import com.fimo.aidentist.ui.auth.LoginActivity
 import com.fimo.aidentist.ui.doctor.DoctorActivity
 import com.fimo.aidentist.ui.treatment.DailyTreatmentActivity
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [HomeFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class HomeFragment : Fragment(), DialogInterface.OnClickListener {
 
     private var _binding: FragmentHomeBinding? = null
     private val binding get() = _binding!!
-    private var param1: String? = null
-    private var param2: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-
-        }
-
-
-
     }
 
     override fun onCreateView(
@@ -71,25 +51,6 @@ class HomeFragment : Fragment(), DialogInterface.OnClickListener {
         _binding = null
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment HomeFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            HomeFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
 
     override fun onClick(dialog: DialogInterface?, which: Int) {
 
