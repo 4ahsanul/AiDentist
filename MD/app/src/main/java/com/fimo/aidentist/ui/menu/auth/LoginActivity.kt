3416fc2c234f90ee5c1dd62a192def7f22460a13
Filfimo Yulfiz Ahsanul Hulqi <58451478+4ahsanul@users.noreply.github.com>
2022-06-04 17:24:35 +0700

    Membuat warning pada sign up form jika input tidak sesuai

--- MD/app/src/main/java/com/fimo/aidentist/ui/menu/auth/LoginActivity.kt
+++ MD/app/src/main/java/com/fimo/aidentist/ui/menu/auth/LoginActivity.kt
@@ -3,6 +3,7 @@ package com.fimo.aidentist.ui.menu.auth
 import android.content.Intent
 import android.os.Build
 import android.os.Bundle
+import android.util.Patterns
 import android.view.WindowInsets
 import android.view.WindowManager
 import android.widget.Toast
--- MD/app/src/main/java/com/fimo/aidentist/ui/menu/auth/SignUpActivity.kt
+++ MD/app/src/main/java/com/fimo/aidentist/ui/menu/auth/SignUpActivity.kt
@@ -2,10 +2,11 @@ package com.fimo.aidentist.ui.menu.auth
 
 import android.content.Intent
 import android.os.Build
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Patterns
 import android.view.WindowInsets
 import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
 import com.fimo.aidentist.databinding.ActivitySignUpBinding
 
 class SignUpActivity : AppCompatActivity() {
@@ -17,11 +18,65 @@ class SignUpActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.tvLogin.setOnClickListener {
-            startActivity(Intent(this,LoginActivity::class.java))
+            startActivity(Intent(this, LoginActivity::class.java))
             finish()
         }
 
+        emailFocusListener()
+        passwordFocusListener()
+        phoneFocusListener()
         setupView()
+    }
+
+    private fun emailFocusListener() {
+        binding.emailEditText.setOnFocusChangeListener { _, focused ->
+            if (!focused) {
+                binding.emailEditTextLayout.helperText = validEmail()
+            }
+        }
+    }
+
+    private fun validEmail(): String? {
+        val emailText = binding.emailEditText.text.toString()
+        if (!Patterns.EMAIL_ADDRESS.matcher(emailText).matches()) {
+            return "Invalid Email Address"
+        }
+        return null
+    }
+
+    private fun passwordFocusListener() {
+        binding.passwordEditText.setOnFocusChangeListener { _, focused ->
+            if (!focused) {
+                binding.passwordEditTextLayout.helperText = validPassword()
+            }
+        }
+    }
+
+    private fun validPassword(): String? {
+        val passwordText = binding.passwordEditText.text.toString()
+        if (passwordText.length < 8) {
+            return "Password terlalu pendek"
+        }
+        return null
+    }
+
+    private fun phoneFocusListener(){
+        binding.phoneEditText.setOnFocusChangeListener { _, focused ->
+            if (!focused) {
+                binding.passwordEditTextLayout.helperText = validPhone()
+            }
+        }
+    }
+
+    private fun validPhone(): String? {
+        val phoneText = binding.phoneEditText.text.toString()
+        if (phoneText.length > 12 || phoneText.length > 13) {
+            return "Pastikan nomer sesuai"
+        }
+        if (!phoneText.matches(".*[0-9].*".toRegex())) {
+            return "Pastikan nomer sesuai"
+        }
+        return null
     }
 
     private fun setupView() {
