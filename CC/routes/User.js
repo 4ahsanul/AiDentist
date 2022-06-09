f5dc6944c3eb152d4c03ac8320c0688632b4f383
Muhammad Zulfikar Novaly Rahman <97008598+novaly479@users.noreply.github.com>
2022-06-10 00:05:36 +0800

    Delete CC/routes directory

--- CC/routes/User.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { DaftarUser, LoginUser } = require('../controller/userCotroller');
-router.post('/daftar', DaftarUser);
-router.post('/login', LoginUser);
-
-module.exports = router;
