8b05c794a1ec8c672743641079144ee9d5a27997
Filfimo Yulfiz Ahsanul Hulqi <58451478+4ahsanul@users.noreply.github.com>
2022-06-09 09:21:51 +0700

    Mendeskripsikan string.XML part 1

--- MD/app/src/main/java/com/fimo/aidentist/ui/navigation/home/HomeFragment.kt
+++ MD/app/src/main/java/com/fimo/aidentist/ui/navigation/home/HomeFragment.kt
@@ -9,7 +9,6 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
 import com.fimo.aidentist.databinding.FragmentHomeBinding
 import com.fimo.aidentist.ui.menu.doctor.DoctorActivity
 import com.fimo.aidentist.ui.menu.treatment.DailyTreatmentActivity
