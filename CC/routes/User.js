4d77036e0ae69b57fa8c51cd886bcfa132484240
A Muh Fitrah Asfar <fitrah4551@gmail.com>
2022-06-10 23:06:24 +0800

    Clear

--- CC/routes/User.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { DaftarUser, LoginUser, profileUser } = require('../controller/userCotroller');
-router.post('/daftar', DaftarUser);
-router.post('/login', LoginUser);
-// router.get('/profile', profileUser);
-
-module.exports = router;
