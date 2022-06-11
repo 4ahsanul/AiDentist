5d5165920de007c8d9f176e29725cabbb8fa941f
Filfimo Yulfiz Ahsanul Hulqi <58451478+4ahsanul@users.noreply.github.com>
2022-06-11 21:18:44 +0700

    Preference sudah login atau belum

--- MD/app/src/main/java/com/fimo/aidentist/ui/boarding/BoardingActivity.kt
+++ MD/app/src/main/java/com/fimo/aidentist/ui/boarding/BoardingActivity.kt
@@ -1,10 +1,18 @@
 package com.fimo.aidentist.ui.boarding
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.preferencesDataStore
 import com.fimo.aidentist.MainActivity
+import com.fimo.aidentist.data.local.UserPreference
+import com.fimo.aidentist.data.model.UserViewModel
+import com.fimo.aidentist.data.model.ViewModelFactory
 import com.fimo.aidentist.databinding.ActivityBoardingBinding
 import com.fimo.aidentist.ui.menu.auth.LoginActivity
 import com.google.firebase.auth.FirebaseAuth
@@ -12,33 +20,35 @@ import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 
+private val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "settings")
+
 class BoardingActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityBoardingBinding
-    private lateinit var auth: FirebaseAuth
+    private lateinit var preference: UserPreference
+
+    private val welcomeViewModel: UserViewModel by viewModels { ViewModelFactory.getInstance() }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val splashScreen = installSplashScreen()
         binding = ActivityBoardingBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        preference = UserPreference.getInstance(dataStore)
+
+        welcomeViewModel.loadUser(preference).observe(this) { pref ->
+            if (pref.isLogin) moveToMainActivity()
+        }
+
         supportActionBar?.hide()
-        auth = Firebase.auth
         setupAction()
     }
 
-    private fun updateUI(currentUser: FirebaseUser?) {
-        if (currentUser != null) {
-            startActivity(Intent(this, MainActivity::class.java))
-            finish()
-        }
-    }
-
-    override fun onStart() {
-        super.onStart()
-        // Check if user is signed in (non-null) and update UI accordingly.
-        val currentUser = auth.currentUser
-        updateUI(currentUser)
+    private fun moveToMainActivity() {
+        val intent = Intent(this, MainActivity::class.java)
+        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        startActivity(intent)
+        finish()
     }
 
     private fun setupAction() {
--- MD/app/src/main/java/com/fimo/aidentist/ui/navigation/profile/ProfileFragment.kt
+++ MD/app/src/main/java/com/fimo/aidentist/ui/navigation/profile/ProfileFragment.kt
@@ -1,25 +1,38 @@
 package com.fimo.aidentist.ui.navigation.profile
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.preferencesDataStore
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import com.fimo.aidentist.MainActivity
+import com.fimo.aidentist.data.local.UserPreference
+import com.fimo.aidentist.data.model.UserViewModel
+import com.fimo.aidentist.data.model.ViewModelFactory
 import com.fimo.aidentist.databinding.FragmentProfileBinding
 import com.fimo.aidentist.helper.Constant
 import com.fimo.aidentist.helper.PreferenceHelper
 import com.fimo.aidentist.ui.menu.auth.LoginActivity
+import com.fimo.aidentist.ui.menu.treatment.DailyTreatmentActivity
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 
+private val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "settings")
+
 class ProfileFragment : Fragment() {
-    private lateinit var sharedPref: PreferenceHelper
     private var _binding: FragmentProfileBinding? = null
     private val binding get() = _binding!!
-    private lateinit var fAuth: FirebaseAuth
+
+    private lateinit var preference: UserPreference
+    private val userViewModel: UserViewModel by viewModels { ViewModelFactory.getInstance() }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -27,17 +40,22 @@ class ProfileFragment : Fragment() {
     ): View {
         // Inflate the layout for this fragment
         _binding = FragmentProfileBinding.inflate(layoutInflater, container, false)
-        sharedPref = PreferenceHelper(requireActivity())
-        fAuth = Firebase.auth
         binding.buttonLogout.setOnClickListener {
-            sharedPref.put(Constant.PREF_IS_LOGIN, false)
-            val intent = Intent(activity, LoginActivity::class.java)
-            startActivity(intent)
-            Toast.makeText(activity, "LOGOUT PREFERENCE SUCCESS", Toast.LENGTH_SHORT).show()
-            sharedPref.clear()
-            fAuth.signOut()
-            activity?.finish()
+            userViewModel.logout(preference)
+            val intent = Intent(requireContext(), LoginActivity::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            requireActivity().startActivity(intent)
+            requireActivity().finish()
         }
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        preference = UserPreference.getInstance(requireContext().dataStore)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 }
