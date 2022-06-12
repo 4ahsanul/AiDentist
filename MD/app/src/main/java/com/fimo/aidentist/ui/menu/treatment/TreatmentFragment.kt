01f2f5436bcd90cb3233b91bfacd69e6363b3f1a
Filfimo Yulfiz Ahsanul Hulqi <58451478+4ahsanul@users.noreply.github.com>
2022-06-12 10:50:21 +0700

    Metode replace fragment pada Self Care [FIXED]

--- MD/app/src/main/java/com/fimo/aidentist/ui/menu/treatment/TreatmentFragment.kt
+++ MD/app/src/main/java/com/fimo/aidentist/ui/menu/treatment/TreatmentFragment.kt
@@ -1,5 +1,6 @@
 package com.fimo.aidentist.ui.menu.treatment
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -7,10 +8,13 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.fimo.aidentist.MainActivity
 import com.fimo.aidentist.R
 import com.fimo.aidentist.databinding.FragmentAnalisisBinding
 import com.fimo.aidentist.databinding.FragmentTreatmentBinding
 import com.fimo.aidentist.ui.adapter.DateAdapter
+import com.fimo.aidentist.ui.menu.doctor.DoctorActivity
+import com.fimo.aidentist.ui.navigation.camera.CameraResultActivity
 
 class TreatmentFragment : Fragment() {
     private lateinit var adapter: DateAdapter
@@ -19,6 +23,10 @@ class TreatmentFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
         adapter = DateAdapter()
         binding.rvDate.apply {
             layoutManager = LinearLayoutManager(context,
@@ -35,6 +43,10 @@ class TreatmentFragment : Fragment() {
         _binding = FragmentTreatmentBinding.inflate(inflater, container, false)
         val view = binding.root
 
+        binding.buttonBack.setOnClickListener {
+            val intent = Intent(activity, MainActivity::class.java)
+            activity?.startActivity(intent)
+        }
         return view
     }
 }
